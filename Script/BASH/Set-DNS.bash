8eec260d9757f8806d463b70a323480b50666044
بی‌ـدار b-daarr <altoni00161514@gmail.com>
2024-08-17 05:57:44 +0330

    Update Set-DNS.bash

--- Script/BASH/Set-DNS.bash
+++ Script/BASH/Set-DNS.bash
@@ -5,3 +5,4 @@ nmcli connection show
 nmcli connection modify "{Connection-Name}" ipv4.dns "{Preferred and Alternate IP like: 0.0.0.0 0.0.0.1}"
 nmcli connection modify "{Connection-Name}" ipv4.ignore-auto-dns yes
 nmcli connection up "{Connection-Name}"
+nmcli connection reload
